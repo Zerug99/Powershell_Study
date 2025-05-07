43cc6154d594f45762e3ed3ff8a570c1cd7cd372
unknown <patryk-99@hotmail.com>
2025-05-07 09:50:21 +0200

    dividering

---
+++ felhantering.ps1
@@ -0,0 +1,19 @@
+# dividera tak 
+
+
+# Funktion, felhnatering 
+
+function Dividera {
+    param ($a, $b)
+    try {
+    $resultat = $a/$b
+    write-host "Resultat: $resultat"
+    }
+    
+    catch {
+        Write-Host "fel $_"
+     }
+    
+}
+
+Dividera -a 10 -b 0
