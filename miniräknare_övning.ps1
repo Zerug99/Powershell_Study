ad3754ac6e127ed3f4e5263caa21b4881221d507
Patryk Jersak <108696082+Zerug99@users.noreply.github.com>
2025-05-12 10:26:37 +0200

    Add files via upload

---
+++ miniräknare_övning.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "Give me number 1" 
+
+[int32]$Number1 = Read-Host
+
+Write-Host " Give me number 2 " 
+
+[int32]$Number2 = Read-Host
+
+$Total = $Number1 + $Number2
+
+Write-Host "The total of" $Number1 + $Number2 "is" $total
