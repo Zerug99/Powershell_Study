b54094a9ffb5d7c17876d5ada7a1049b087d02d3
Patryk Jersak <108696082+Zerug99@users.noreply.github.com>
2025-05-12 10:25:00 +0200

    Add files via upload

---
+++ getservice.ps1
@@ -0,0 +1,4 @@
+﻿Write-host "Give me your name of the service" #ange rätt variabel på property namn som services har
+$service = Read-Host
+
+Get-Service -Name $service # Har ´man angett spooler så blir det samma sak som Get-Service -Name "spooler" 
